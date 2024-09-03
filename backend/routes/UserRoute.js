4639227a4a90012c8d6364f46b2e42ac05e6dac1
Achmad Rangga <119169812+Lucifashz@users.noreply.github.com>
2024-09-03 12:49:12 +0700

    Create UserRoute.js

---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,37 @@
+import express from "express";
+import User from "../models/UserModel.js";
+
+
+const router = express.Router();
+
+router.get('/users', async (req, res) => {
+	await User.find({}, { _id: 1, name: 1, username: 1, email: 1, partnerId: 1 })
+	.then((result) => {
+		res.json(result)
+	})
+	.catch((error) => {
+		res.status(500).json({message: error.message});
+	}));
+router.get('/users/:id', (req, res) => {
+   res.send(process.env.PAYPAL_CLIENT_ID);
+});
+router.patch('/add-partner/:id', (req, res) => {
+   res.send(process.env.PAYPAL_CLIENT_ID);
+});
+router.patch('/delete-partner/:id', (req, res) => {
+   res.send(process.env.PAYPAL_CLIENT_ID);
+});
+router.post('/register', (req, res) => {
+   res.send(process.env.PAYPAL_CLIENT_ID);
+});
+router.post('/login', (req, res) => {
+   res.send(process.env.PAYPAL_CLIENT_ID);
+});
+router.delete('/logout', (req, res) => {
+   res.send(process.env.PAYPAL_CLIENT_ID);
+});
+router.get('/token', (req, res) => {
+   res.send(process.env.PAYPAL_CLIENT_ID);
+});
+
+export default router;
