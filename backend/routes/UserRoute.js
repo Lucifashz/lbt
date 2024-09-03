958a6eb59ef6b5e6407a8b0f2f9f6df6f2759f51
Achmad Rangga <119169812+Lucifashz@users.noreply.github.com>
2024-09-03 13:17:15 +0700

    Update UserRoute.js

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -21,9 +21,42 @@ router.get('/users/:id', async (req, res) => {
 		res.status(404).json({message: error.message});
 	}
 });
-router.patch('/add-partner/:id', (req, res) => {
-   res.send(process.env.PAYPAL_CLIENT_ID);
+
+
+router.patch('/add-partner/:id', async (req, res) => {
+   try {
+      const user = await User.findById(req.params.id);
+      const partner = await User.findById(req.body["partner"]);
+
+      // Check if partner is provided in the request
+      if (typeof req.body["partner"] === 'undefined') {
+         return res.status(401).send({ message: "Partner tidak ditemukan" });
+      }
+
+      // Prevent user from adding themselves as a partner
+      if (req.params.id === req.body["partner"]) {
+         return res.status(401).send({ message: "Tidak bisa menambahkan diri kamu sebagai partner" });
+      }
+
+      // Check if both users do not already have a partner
+      if (user?.partnerId || partner?.partnerId) {
+         return res.status(401).send({ message: "Kamu atau keduanya sudah punya partner" });
+      }
+
+      // Update both users with each other's partnerId
+      await User.updateOne({ _id: req.params.id }, { $set: { partnerId: req.body["partner"] } });
+      await User.updateOne({ _id: req.body["partner"] }, { $set: { partnerId: req.params.id } });
+
+      // Send a success response after both updates
+      res.status(200).json({ message: "Partner berhasil ditambahkan" });
+
+   } catch (error) {
+      // Catch and handle any errors
+      res.status(500).send({ message: error.message });
+   }
 });
+
+
 router.patch('/delete-partner/:id', (req, res) => {
    res.send(process.env.PAYPAL_CLIENT_ID);
 });
