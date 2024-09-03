88dba27c7fbabe04a957d1b8398309a14ace8b6d
Achmad Rangga <119169812+Lucifashz@users.noreply.github.com>
2024-09-03 14:07:56 +0700

    Update UsersController.js

--- backend/controllers/UsersController.js
+++ backend/controllers/UsersController.js
@@ -5,11 +5,10 @@ import validator from "validator";
 
 
 export const getUsers = async (req, res) => {
-	await User.find({}, { _id: 1, name: 1, username: 1, email: 1, partnerId: 1 })
-	.then((result) => {
-		res.json(result)
-	})
-	.catch((error) => {
-		res.status(500).json({message: error.message});
-	});
+	try {
+		const users = await User.find({}, { _id: 1, name: 1, username: 1, email: 1, partnerId: 1 })
+		res.json(users);
+	} catch (error) {
+		res.status(404).json({message: error.message});
+	}
 }
