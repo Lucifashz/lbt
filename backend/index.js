1875071380c68ce758d69bdf9b4fadb97f03b884
Achmad Rangga <119169812+Lucifashz@users.noreply.github.com>
2024-09-03 18:51:15 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,21 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.use(express.static('public'));
+
+app.get('/cookie', (req, res) => {
+  let response;
+
+  if (req.cookies?.somecookie) {
+    response = 'Same cookie: A cookie received and the same sent to client';
+  } else {
+    res.cookie('somecookie', 'cookie text');
+    response = 'New cookie: A new cookie created and sent to the client';
+  }
+
+  res.send(response);
+});
+
 
 // Matches
 app.use(MatchRoute);
