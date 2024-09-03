be316d8974a6bb253805e96b41fea3ee28ae26f0
Achmad Rangga <119169812+Lucifashz@users.noreply.github.com>
2024-09-03 13:11:59 +0700

    Update UserRoute.js

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,19 +1,19 @@
 import express from "express";
-import { getUsers } from "../controllers/UsersController.js";
+import User from "../models/UserModel.js";
 
 
 const router = express.Router();
 
-// router.get('/users', async (req, res) => {
-// 	await User.find({}, { _id: 1, name: 1, username: 1, email: 1, partnerId: 1 })
-// 	.then((result) => {
-// 		res.json(result)
-// 	})
-// 	.catch((error) => {
-// 		res.status(500).json({message: error.message});
-// 	});
-// });
-router.get('/users', getUsers);
+router.get('/users', async (req, res) => {
+	await User.find({}, { _id: 1, name: 1, username: 1, email: 1, partnerId: 1 })
+	.then((result) => {
+		res.json(result)
+	})
+	.catch((error) => {
+		res.status(500).json({message: error.message});
+	});
+});
+
 router.get('/users/:id', (req, res) => {
    res.send(process.env.PAYPAL_CLIENT_ID);
 });
