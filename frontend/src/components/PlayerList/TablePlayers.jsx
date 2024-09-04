cbccd0f4016f3392b5697242257fd4a36745068b
Achmad Rangga <119169812+Lucifashz@users.noreply.github.com>
2024-09-04 14:18:22 +0700

    Update TablePlayers.jsx

--- frontend/src/components/PlayerList/TablePlayers.jsx
+++ frontend/src/components/PlayerList/TablePlayers.jsx
@@ -2,63 +2,25 @@ import React from "react";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import { Link } from "react-router-dom";
-import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-  Input,
-  Button,
-  DropdownTrigger,
-  Dropdown,
-  DropdownMenu,
-  DropdownItem,
-  User,
-  Pagination,
-  Select,
-  SelectItem,
-} from "@nextui-org/react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Pagination, getKeyValue} from "@nextui-org/react";
 import ChallengeCard from "./ChallengeCard";
 import PartnerCard from "./PartnerCard";
-import {SearchIcon} from "./SearchIcon";
-import {ChevronDownIcon} from "./ChevronDownIcon";
-import {columns, users} from "./data";
-import {capitalize} from "./utils";
-
-const rowPages = [
-  {key: "5", label: "5"},
-  {key: "10", label: "10"},
-  {key: "15", label: "15"},
-  {key: "20", label: "20"},
-  {key: "25", label: "25"},
-  {key: "30", label: "30"},
-];
+import {users} from "./data";
+import { X, Search  } from 'lucide-react';
 
 
-const INITIAL_VISIBLE_COLUMNS = ["name", "username", "email", "actions"];
 
 export default function App() {
-  const [filterValue, setFilterValue] = React.useState("");
-  const [visibleColumns, setVisibleColumns] = React.useState(new Set(INITIAL_VISIBLE_COLUMNS));
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const [sortDescriptor, setSortDescriptor] = React.useState({
-    column: "age",
-    direction: "ascending",
-  });
-  const [page, setPage] = React.useState(1);
-
   const [players, setPlayers] = React.useState(users);
   const [playerLogin, setPlayerLogin] = React.useState({});
-
   const [token, setToken] = React.useState("");
   const [expire, setExpire] = React.useState("");
+  const [search, setSearch] = React.useState("");
 
   React.useEffect(() => {
       refreshToken();
       getPlayers();
-  }, []);
+  }, [search]);
 
   // Agar axios dapat membaca cookies
   axios.defaults.withCredentials = true;
@@ -86,249 +48,101 @@ export default function App() {
       })
   }
 
+  const [page, setPage] = React.useState(1);
+  const rowsPerPage = 10;
+
   const pages = Math.ceil(players.length / rowsPerPage);
-
-  const hasSearchFilter = Boolean(filterValue);
-
-  const headerColumns = React.useMemo(() => {
-    if (visibleColumns === "all") return columns;
-
-    return columns.filter((column) => Array.from(visibleColumns).includes(column.uid));
-  }, [visibleColumns]);
-
-  const filteredItems = React.useMemo(() => {
-    let filteredUsers = [...players];
-
-    if (hasSearchFilter) {
-      filteredUsers = filteredUsers.filter((player) =>
-        player.name.toLowerCase().includes(filterValue.toLowerCase()) ||
-        player.username.toLowerCase().includes(filterValue.toLowerCase())
-      );
-    }
-
-    return filteredUsers;
-  }, [players, filterValue]);
 
   const items = React.useMemo(() => {
     const start = (page - 1) * rowsPerPage;
     const end = start + rowsPerPage;
 
-    return filteredItems.slice(start, end);
-  }, [page, filteredItems, rowsPerPage]);
-
-  const sortedItems = React.useMemo(() => {
-    return [...items].sort((a, b) => {
-      const first = a[sortDescriptor.column];
-      const second = b[sortDescriptor.column];
-      const cmp = first < second ? -1 : first > second ? 1 : 0;
-
-      return sortDescriptor.direction === "descending" ? -cmp : cmp;
-    });
-  }, [sortDescriptor, items]);
-
-  const renderCell = React.useCallback((player, columnKey, login) => {
-    const cellValue = player[columnKey];
-    switch (columnKey) {
-      case "name":
-        return (
-          <User
-            avatarProps={{radius: "full", size: "sm", src: player.avatar}}
-            classNames={{
-              description: "text-default-500 text-sm",
-            }}
-            description={player.username}
-            name={cellValue}
-          >
-            {player.email}
-          </User>
-        );
-      case "username":
-        return (
-          <div className="flex flex-col">
-            <p className="text-bold text-sm text-default-500">{player.username}</p>
-          </div>
-        );
-      case "actions":
-        return (
-          <div className="relative flex justify-end items-center gap-2">
-            { 
-              player._id !== login.partnerId && login.userId ? 
-              <ChallengeCard player={player} playerLogin={login}/> :
-              ""
-            }
-
-            {player._id !== login.partnerId && login.userId && player.partnerId === "" && login.partnerId === "" ? <PartnerCard player={player} playerLogin={login}/> : ""}
-            <Link to={`/users/${player._id}`} className="py-2.5 px-5 text-sm font-medium rounded-lg bg-cyan-950 text-white shadow-sm hover:bg-cyan-900">
-              Lihat Profil
-            </Link>
-          </div>
-        );
-      default:
-        return cellValue;
-    }
-  }, []);
-
-  const onRowsPerPageChange = React.useCallback((e) => {
-    setRowsPerPage(Number(e.target.value));
-    setPage(1);
-  }, []);
-
-
-  const onSearchChange = React.useCallback((value) => {
-    if (value) {
-      setFilterValue(value);
-      setPage(1);
-    } else {
-      setFilterValue("");
-    }
-  }, []);
-
-  const topContent = React.useMemo(() => {
-    return (
-      <div className="flex flex-col gap-4">
-        <div className="flex justify-between gap-3 items-end">
-          <Input
-            isClearable
-            classNames={{
-              base: "w-full sm:max-w-[44%]",
-              inputWrapper: "border-1",
-            }}
-            placeholder="Search by name or username..."
-            size="sm"
-            startContent={<SearchIcon className="text-default-300" />}
-            value={filterValue}
-            variant="bordered"
-            onClear={() => setFilterValue("")}
-            onValueChange={onSearchChange}
-          />
-          <div className="flex gap-3">
-            <Dropdown>
-              <DropdownTrigger className="hidden sm:flex">
-                <Button
-                  endContent={<ChevronDownIcon className="text-small" />}
-                  size="sm"
-                  variant="flat"
-                >
-                  Columns
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                disallowEmptySelection
-                aria-label="Table Columns"
-                closeOnSelect={false}
-                selectedKeys={visibleColumns}
-                selectionMode="multiple"
-                onSelectionChange={setVisibleColumns}
-              >
-                {columns.map((column) => (
-                  <DropdownItem key={column.uid} className="capitalize">
-                    {capitalize(column.name)}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-          </div>
-        </div>
-        <div className="flex justify-between items-center">
-          <span className="text-default-400 text-small">Total {players.length} users</span>
-          <Select
-          items={rowPages}
-          label="Rows per page:"
-          placeholder="5"
-          className="max-w-xs"
-          size="sm"
-          onChange={onRowsPerPageChange}
-        >
-          {(rowPage) => <SelectItem>{rowPage.label}</SelectItem>}
-        </Select>
-        </div>
-      </div>
+    const searchByNameOrUsername = players.filter((player) => 
+      player.name.toLowerCase().includes(search.toLowerCase()) || 
+      player.username.toLowerCase().includes(search.toLowerCase())
     );
-  }, [
-    filterValue,
-    visibleColumns,
-    onSearchChange,
-    onRowsPerPageChange,
-    players.length,
-    hasSearchFilter,
-  ]);
 
-  const bottomContent = React.useMemo(() => {
-    return (
-      <div className="py-2 px-2 flex justify-between items-center">
-        <Pagination
-          showControls
-          classNames={{
-            cursor: "bg-foreground text-background",
-          }}
-          color="default"
-          isDisabled={hasSearchFilter}
-          page={page}
-          total={pages}
-          variant="light"
-          onChange={setPage}
-        />
-      </div>
-    );
-  }, [items.length, page, pages, hasSearchFilter]);
+    return searchByNameOrUsername.slice(start, end);
+  }, [page, players]);
 
-  const classNames = React.useMemo(
-    () => ({
-      wrapper: ["max-h-[382px]", "max-w-3xl"],
-      th: ["bg-transparent", "text-default-500", "border-b", "border-divider"],
-      td: [
-        // changing the rows border radius
-        // first
-        "group-data-[first=true]:first:before:rounded-none",
-        "group-data-[first=true]:last:before:rounded-none",
-        // middle
-        "group-data-[middle=true]:before:rounded-none",
-        // last
-        "group-data-[last=true]:first:before:rounded-none",
-        "group-data-[last=true]:last:before:rounded-none",
-      ],
-    }),
-    [],
-  );
+
+    const renderCell = React.useCallback((player, columnKey, login) => {
+      const cellValue = player[columnKey];
+      switch (columnKey) {
+        case "actions":
+          return (
+            <div className="relative flex justify-end items-center gap-2">
+              { 
+                player._id !== login.partnerId && login.userId ? 
+                <ChallengeCard player={player} playerLogin={login}/> :
+                ""
+              }
+
+              {player._id !== login.partnerId && login.userId && player.partnerId === "" && login.partnerId === "" ? <PartnerCard player={player} playerLogin={login}/> : ""}
+              <Link to={`/users/${player._id}`} className="py-2.5 px-5 text-sm font-medium rounded-lg bg-cyan-950 text-white shadow-sm hover:bg-cyan-900">
+                Lihat Profil
+              </Link>
+            </div>
+          );
+        default:
+          return cellValue;
+      }
+  }, []);
+
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+    };
 
   return (
-    <Table
-      isCompact
-      removeWrapper
-      aria-label="Example table with custom cells, pagination and sorting"
-      bottomContent={bottomContent}
-      bottomContentPlacement="outside"
-      checkboxesProps={{
-        classNames: {
-          wrapper: "after:bg-foreground after:text-background text-background",
-        },
+    <Table 
+      aria-label="Example table with client side pagination"
+      topContent={
+        <div>
+          <div className="relative z-0" >
+              <input type="text" id="search" className={`block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-1 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer`} placeholder=" " name="search" onChange={handleSearch} value={search} required/>
+              <label htmlFor="search" className={`absolute text-sm  duration-300 transform -translate-y-4 scale-75 top-2 ${search ? "z-10" : "-z-10"} origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:z-10 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1`}>Cari pemain berdasarkan nama atau username</label>
+                {
+                    search ?
+                    <button onClick={() => setSearch("")} type="button" className="absolute inset-y-0 end-0 flex items-center z-20 px-3 cursor-pointer text-gray-400 rounded-e-md focus:outline-none focus:text-blue-600">
+                      <X className="size-3.5"/>
+                    </button> :
+                    <div className="absolute inset-y-0 end-0 flex items-center z-20 px-3 text-gray-400 rounded-e-md focus:outline-none focus:text-blue-600">
+                      <Search className="size-3.5"/>
+                    </div>
+                }
+          </div>
+        </div>
+      }
+      bottomContent={
+        <div className="flex w-full justify-center">
+          <Pagination
+            isCompact
+            showControls
+            showShadow
+            color="secondary"
+            page={page}
+            total={pages}
+            onChange={(page) => setPage(page)}
+          />
+        </div>
+      }
+      classNames={{
+        wrapper: "min-h-[222px]",
       }}
-      classNames={classNames}
-      sortDescriptor={sortDescriptor}
-      topContent={topContent}
-      topContentPlacement="outside"
-      onSortChange={setSortDescriptor}
     >
-      <TableHeader columns={headerColumns}>
-        {(column) => (
-          <TableColumn
-            key={column.uid}
-            align={column.uid === "actions" ? "center" : "start"}
-            allowsSorting={column.sortable}
-          >
-            {column.name}
-          </TableColumn>
-        )}
+      <TableHeader>
+        <TableColumn key="name">NAME</TableColumn>
+        <TableColumn key="username">USERNAME</TableColumn>
+        <TableColumn key="email">EMAIL</TableColumn>
+        <TableColumn key="actions">ACTION</TableColumn>
       </TableHeader>
-      <TableBody emptyContent={"No users found"} items={sortedItems}>
+      <TableBody items={items}>
         {(item) => (
-          item._id !== playerLogin.userId ?
-          <TableRow key={item._id}>
+          <TableRow key={item.name}>
             {(columnKey) => <TableCell>{renderCell(item, columnKey, playerLogin)}</TableCell>}
           </TableRow>
-          : ""
         )}
       </TableBody>
     </Table>
-  );
+  )
 }
