ef833892255c21ccd663680c8735cdff08762fbb
Achmad Rangga <119169812+Lucifashz@users.noreply.github.com>
2024-09-04 14:17:57 +0700

    Update PlayerList.jsx

--- frontend/src/pages/PlayerList.jsx
+++ frontend/src/pages/PlayerList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NavbarHeader from "../components/Navbar/NavbarHeader"
-import { Card } from "@nextui-org/react";
 import TablePlayers from "../components/PlayerList/TablePlayers";
 
 export default function PlayerList() {
@@ -10,9 +9,7 @@ export default function PlayerList() {
          <NavbarHeader/>
          <div className="mt-6 flex w-full h-auto justify-center">
             <div className="flex px-6 py-6 gap-4 w-full flex-col max-w-[1280px]">
-               <Card className="p-3 overflow-scroll md:overflow-hidden">
                   <TablePlayers/>
-               </Card>
             </div>
          </div>
       </>
