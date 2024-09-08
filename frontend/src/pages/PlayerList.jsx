aec6c5b6cda4febbb0b9d5c638e1162a0532e872
Achmad Rangga <119169812+Lucifashz@users.noreply.github.com>
2024-09-08 14:56:32 +0700

    Update PlayerList.jsx

--- frontend/src/pages/PlayerList.jsx
+++ frontend/src/pages/PlayerList.jsx
@@ -9,6 +9,7 @@ export default function PlayerList() {
          <NavbarHeader/>
          <div className="mt-6 flex w-full h-auto justify-center">
             <div className="flex px-6 py-6 gap-4 w-full flex-col max-w-[1280px]">
+                  <p>ada</p>
                   <TablePlayers/>
             </div>
          </div>
